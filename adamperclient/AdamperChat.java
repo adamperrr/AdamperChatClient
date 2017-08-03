2544f5e02a1d30cfcb3b2835894ecbfbadb606d7
Adamperrr <adam.pertek@poczta.fm>
2017-08-03 16:05:31 +0200

    Send messages on clicking enter, color sended messages

--- adamperclient/AdamperChat.java
+++ adamperclient/AdamperChat.java
@@ -3,6 +3,10 @@ package adamperclient;
 import java.util.*;
 import java.io.*;
 import java.net.*;
+import javax.swing.text.*;
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class AdamperChat extends javax.swing.JFrame {
 
@@ -48,77 +52,124 @@ public class AdamperChat extends javax.swing.JFrame {
    */
   public AdamperChat() {
     initComponents();
+    this.getRootPane().setDefaultButton(sendBtn); // Wyślij jako główny przycisk
+  }
+
+  public void appendMsg(String message) {
+    StyledDocument doc = mainTextArea.getStyledDocument();
+    message = message.trim() + "\n";
+    try {
+      doc.insertString(doc.getLength(), message, null);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public void appendUserMsg(String username, String message) {
+    StyledDocument doc = mainTextArea.getStyledDocument();
+    username = username.trim() + ": ";
+    message = message.trim() + "\n";
+
+    SimpleAttributeSet keyWord = new SimpleAttributeSet();
+    StyleConstants.setBold(keyWord, true);
+
+    try {
+      doc.insertString(doc.getLength(), username, keyWord);
+      doc.insertString(doc.getLength(), message, null);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public void appendThisUserMsg(String message) {
+    StyledDocument doc = mainTextArea.getStyledDocument();
+    String username = _username.trim() + ": ";
+    message = message.trim() + "\n";
+
+    SimpleAttributeSet keyWord = new SimpleAttributeSet();
+    StyleConstants.setForeground(keyWord, Color.BLUE);
+    StyleConstants.setBold(keyWord, true);
+
+    try {
+      doc.insertString(doc.getLength(), username, keyWord);
+      doc.insertString(doc.getLength(), message, null);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
   }
 
   public void connect() {
-    if (isConnected == false) {
+    if (_isConnected == false) {
       try {
-        sock = new Socket(address, port);
-        InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
-        reader = new BufferedReader(streamreader);
-        writer = new PrintWriter(sock.getOutputStream());
-        writer.println(username + ":has connected.:Connect");
-        writer.flush();
-        isConnected = true;
-      } 
-	  catch (Exception ex) {
-        mainTextArea.append("Cannot Connect! Try Again. \n");
+        _sock = new Socket(_address, _port);
+        InputStreamReader streamreader = new InputStreamReader(_sock.getInputStream());
+        _reader = new BufferedReader(streamreader);
+        _writer = new PrintWriter(_sock.getOutputStream());
+        _writer.println(_username + ":has connected.:Connect");
+        _writer.flush();
+        _isConnected = true;
+      } catch (Exception ex) {
+        appendMsg("Cannot Connect! Try Again. \n");
       }
 
       ListenThread();
 
-    } else if (isConnected == true) {
-      mainTextArea.append("You are already connected. \n");
+    } else if (_isConnected == true) {
+      appendMsg("You are already connected. \n");
     }
-  }  
-  
+  }
+
   public void disconnect() {
     try {
-      mainTextArea.append("Disconnected.\n");
-      sock.close();
+      appendMsg("Disconnected.\n");
+      _sock.close();
     } catch (Exception ex) {
-      mainTextArea.append("Failed to disconnect. \n");
+      appendMsg("Failed to disconnect. \n");
     }
-    isConnected = false;
+    _isConnected = false;
   }
 
   public void sendDisconnect() {
-    String bye = (username + ": :Disconnect");
+    String bye = (_username + ": :Disconnect");
     try {
-      writer.println(bye);
-      writer.flush();
+      _writer.println(bye);
+      _writer.flush();
     } catch (Exception e) {
-      mainTextArea.append("Could not send Disconnect message.\n");
+      appendMsg("Could not send Disconnect message.\n");
     }
   }
 
-  public void chatMsgIncomingReader(String userName, String msg) {
-    mainTextArea.append(username + ": " + msg + "\n");
-  }  
-  
-  public void connectIncomingReader(String userName) {
+  public void chatMsgIncomingReader(String username, String message) {
+    if (username.equals(_username)) {
+      appendThisUserMsg(message);
+    } else {
+      appendUserMsg(username, message);
+    }
+  }
+
+  public void connectIncomingReader(String username) {
     mainTextArea.removeAll();
-    addUserIncomingReader(userName);
+    addUserIncomingReader(username);
   }
 
-  public void disconnectIncomingReader(String userName) {
-    removeUserIncomingReader(userName);
+  public void disconnectIncomingReader(String username) {
+    removeUserIncomingReader(username);
   }
 
-  public void doneIncomingReader(String userName) {
-    users.clear();
+  public void doneIncomingReader() {
+    _users.clear();
   }
 
-  public void addUserIncomingReader(String userName) {
-    users.add(userName);
+  public void addUserIncomingReader(String username) {
+    _users.add(username);
   }
 
-  public void removeUserIncomingReader(String userName) {
-    mainTextArea.append(userName + " is now offline.\n");
+  public void removeUserIncomingReader(String username) {
+    appendMsg(username + " is now offline.\n");
   }
 
   public String getReaderLine() throws IOException {
-    return reader.readLine();
+    return _reader.readLine();
   }
 
   public void ListenThread() {
@@ -126,7 +177,7 @@ public class AdamperChat extends javax.swing.JFrame {
     Thread IncomingReader = new Thread(tempIR);
     IncomingReader.start();
   }
-  
+
   /**
    * This method is called from within the constructor to initialize the form.
    * WARNING: Do NOT modify this code. The content of this method is always
@@ -136,23 +187,18 @@ public class AdamperChat extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
-    jScrollPane1 = new javax.swing.JScrollPane();
-    mainTextArea = new javax.swing.JTextArea();
     messageTextField = new javax.swing.JTextField();
     logoutBtn = new javax.swing.JButton();
     messageFieldLabel = new javax.swing.JLabel();
     sendBtn = new javax.swing.JButton();
     connectBtn = new javax.swing.JButton();
+    jScrollPane2 = new javax.swing.JScrollPane();
+    mainTextArea = new javax.swing.JTextPane();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     setTitle("Chat - Adamper");
     setMinimumSize(new java.awt.Dimension(410, 320));
     setPreferredSize(new java.awt.Dimension(410, 320));
-
-    mainTextArea.setEditable(false);
-    mainTextArea.setColumns(20);
-    mainTextArea.setRows(5);
-    jScrollPane1.setViewportView(mainTextArea);
 
     logoutBtn.setText("Wyloguj");
     logoutBtn.addActionListener(new java.awt.event.ActionListener() {
@@ -177,6 +223,8 @@ public class AdamperChat extends javax.swing.JFrame {
       }
     });
 
+    jScrollPane2.setViewportView(mainTextArea);
+
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
@@ -184,7 +232,7 @@ public class AdamperChat extends javax.swing.JFrame {
       .addGroup(layout.createSequentialGroup()
         .addContainerGap()
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-          .addComponent(jScrollPane1)
+          .addComponent(jScrollPane2)
           .addGroup(layout.createSequentialGroup()
             .addComponent(messageTextField)
             .addGap(10, 10, 10)
@@ -207,7 +255,7 @@ public class AdamperChat extends javax.swing.JFrame {
           .addComponent(logoutBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
           .addComponent(connectBtn))
         .addGap(10, 10, 10)
-        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 194, Short.MAX_VALUE)
+        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 194, Short.MAX_VALUE)
         .addGap(10, 10, 10)
         .addComponent(messageFieldLabel)
         .addGap(5, 5, 5)
@@ -218,6 +266,7 @@ public class AdamperChat extends javax.swing.JFrame {
     );
 
     pack();
+    setLocationRelativeTo(null);
   }// </editor-fold>//GEN-END:initComponents
 
   private void logoutBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logoutBtnActionPerformed
@@ -225,17 +274,17 @@ public class AdamperChat extends javax.swing.JFrame {
     disconnect();
   }//GEN-LAST:event_logoutBtnActionPerformed
 
-  private void sendBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendBtnActionPerformed
+  private void sendBtnActionMethod() {
     String nothing = "";
     if ((messageTextField.getText()).equals(nothing)) {
       messageTextField.setText("");
       messageTextField.requestFocus();
     } else {
       try {
-        writer.println(username + ":" + messageTextField.getText() + ":" + "Chat");
-        writer.flush(); // flushes the buffer
+        _writer.println(_username + ":" + messageTextField.getText() + ":" + "Chat");
+        _writer.flush(); // flushes the buffer
       } catch (Exception ex) {
-        mainTextArea.append("Message was not sent. \n");
+        appendMsg("Message was not sent. \n");
       }
       messageTextField.setText("");
       messageTextField.requestFocus();
@@ -243,28 +292,39 @@ public class AdamperChat extends javax.swing.JFrame {
 
     messageTextField.setText("");
     messageTextField.requestFocus();
+  }
+
+  private void sendBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendBtnActionPerformed
+    sendBtnActionMethod();
   }//GEN-LAST:event_sendBtnActionPerformed
 
   private void connectBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connectBtnActionPerformed
     connect();
   }//GEN-LAST:event_connectBtnActionPerformed
 
-  private String username = "NazwaUsera";
-  private String address = "localhost";
+  Random _randGen = new Random();
+  private String _username = "NazwaUsera" + _randGen.nextInt();
+  private String _address = "localhost";
 
-  private ArrayList<String> users = new ArrayList();
-  private int port = 2222;
-  private boolean isConnected = false;
+  private ArrayList<String> _users = new ArrayList();
+  private int _port = 2222;
+  private boolean _isConnected = false;
 
-  private Socket sock;
-  private BufferedReader reader;
-  private PrintWriter writer;
+  private Socket _sock;
+  private BufferedReader _reader;
+  private PrintWriter _writer;
 
+  private Action accept = new AbstractAction("Accept") { // Akcja po kliknięciu enter
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      sendBtnActionMethod();
+    }
+  };
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JButton connectBtn;
-  private javax.swing.JScrollPane jScrollPane1;
+  private javax.swing.JScrollPane jScrollPane2;
   private javax.swing.JButton logoutBtn;
-  private javax.swing.JTextArea mainTextArea;
+  private javax.swing.JTextPane mainTextArea;
   private javax.swing.JLabel messageFieldLabel;
   private javax.swing.JTextField messageTextField;
   private javax.swing.JButton sendBtn;
--- adamperclient/IncomingReader.java
+++ adamperclient/IncomingReader.java
@@ -3,7 +3,7 @@ package adamperclient;
 public class IncomingReader implements Runnable {
 
   public IncomingReader(javax.swing.JFrame form) {
-    mainFrame = (AdamperChat) form;
+    _mainFrame = (AdamperChat) form;
   }
 
   @Override
@@ -12,20 +12,20 @@ public class IncomingReader implements Runnable {
     String stream, done = "Done", connect = "Connect", disconnect = "Disconnect", chat = "Chat";
 
     try {
-      while ((stream = mainFrame.getReaderLine()) != null) {
+      while ((stream = _mainFrame.getReaderLine()) != null) {
         data = stream.split(":");
 
         if (data[2].equals(chat)) {
-          mainFrame.chatMsgIncomingReader(data[0], data[1]);
+          _mainFrame.chatMsgIncomingReader(data[0], data[1]);
         }
         else if (data[2].equals(connect)) {
-          mainFrame.connectIncomingReader(data[0]);
+          _mainFrame.connectIncomingReader(data[0]);
         }
         else if (data[2].equals(disconnect)) {
-          mainFrame.disconnectIncomingReader(data[0]);
+          _mainFrame.disconnectIncomingReader(data[0]);
         }
         else if (data[2].equals(done)) {
-          mainFrame.doneIncomingReader(data[0]);
+          _mainFrame.doneIncomingReader();
         }
       }
     }
@@ -34,6 +34,6 @@ public class IncomingReader implements Runnable {
     }
   }
 
-  private AdamperChat mainFrame;
+  private AdamperChat _mainFrame;
 
 }
